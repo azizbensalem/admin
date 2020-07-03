6cf8dcf2f805c2207e051cf1e304b781a90234c4
Aziz Ben Salem <azizbensalem1998@gmail.com>
2020-07-03 18:42:28 +0100

    Updates

---
+++ precache-manifest.4ba8942dd57bf0cdf469d9b6355eeee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d027ec5386d2f30d980febedac67c9",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "3f96fe595e8a21043e91",
+    "url": "/admin/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b002a8b9f50b49f830c7",
+    "url": "/admin/static/js/2.eda988c6.chunk.js"
+  },
+  {
+    "revision": "f07796f4f30300629a9fa688e41af979",
+    "url": "/admin/static/js/2.eda988c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f96fe595e8a21043e91",
+    "url": "/admin/static/js/main.024da2ba.chunk.js"
+  },
+  {
+    "revision": "cd999041da0e0b357c75",
+    "url": "/admin/static/js/runtime-main.14bebaaf.js"
+  }
+]);
